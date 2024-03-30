0fbbbf9d39aab5f7a2fc585148690341043707fc
minseo Park <eo77si9@naver.com>
2024-03-30 18:22:02 +0900

    Feat Context Print

--- src/pages/ResultRoute/ResultRoute.jsx
+++ src/pages/ResultRoute/ResultRoute.jsx
@@ -3,6 +3,10 @@ import ContentTemplate from 'components/ContentsTemplate';
 import RouteComponent from 'components/ResultRoute/RouteComponent';
 import { useRouteData } from 'contexts/RouteDataContext'
 
+import { useSelectedRegionContext } from 'contexts/SelectedRegionContext';
+import { useSelectedPeriodContext } from 'contexts/SelectedPeriodContext';
+import { useSelectedStrengthContext } from 'contexts/SelectedStrengthContext';
+
 const routes = [
   {
     "number": 0,
@@ -133,6 +137,19 @@ const routes = [
 
 const ResultRoute = () => {
   const { routeData, setRouteData } = useRouteData();
+
+  const { selectedRegion } = useSelectedRegionContext();
+  const { selectedPeriod } = useSelectedPeriodContext();
+  const { selectedStrength } = useSelectedStrengthContext();
+
+  useEffect(() => {
+    // 선택된 데이터를 콘솔에 출력합니다.
+    console.log("선택된 여행지역:", selectedRegion);
+    console.log("선택된 여행기간:", selectedPeriod);
+    console.log("선택된 여행강도:", selectedStrength);
+  }, []);
+
+
   useEffect(() => {
     // do stuff here...
     setRouteData(null);
