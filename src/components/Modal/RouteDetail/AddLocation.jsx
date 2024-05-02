001d6704ff9078a03de4c8839d84aff1f03e2a6c
nayechan <66f@naver.com>
2024-05-02 16:23:38 +0900

    Fetch data from server

--- src/components/Modal/RouteDetail/AddLocation.jsx
+++ src/components/Modal/RouteDetail/AddLocation.jsx
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
+import axios from 'axios';
+
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faEdit, faTrashAlt, faPlus, faFileExport, faFileImport } from '@fortawesome/free-solid-svg-icons';
 import { Map, MapMarker } from 'react-kakao-maps-sdk';
@@ -8,7 +10,7 @@ import DefaultButton from 'components/DefaultButton';
 import Modal from 'components/Modal/Modal';
 import { useCurrentRouteData } from 'contexts/CurrentRouteDataContext';
 
-const {kakao} = window;
+const { kakao } = window;
 
 // Style for modal itself
 const ModalWrapper = styled.div`
@@ -26,6 +28,7 @@ const TabButton = styled.button`
   border-bottom: 2px solid transparent;
   font-size: 16px;
   cursor: pointer;
+  min-width:100px;
   transition: border-color 0.3s ease;
 
   &:hover {
@@ -40,6 +43,8 @@ const TabButton = styled.button`
 
 // Style for button container
 const TabButtonContainer = styled.div`
+  display: flex; /* Use flexbox */
+  overflow-x: auto; /* Enable horizontal scrollbar if content overflows */
 `;
 
 const ContentWrapper = styled.div`
@@ -62,35 +67,50 @@ const ButtonContainer = styled.div`
   justify-content: flex-end;
 `;
 
-const AddLocation = ({ isOpen, setIsOpen, dayIndex, locationIndex }) => {
-  const generateRecommendedLocations = () => {
-    const locations = [];
-    for (let i = 1; i <= 10; i++) {
-      locations.push({
-        id: i,
-        name: `Location ${i}`,
-        address: `Address ${i}`,
-        latitude: 32 + Math.random() * 6,
-        longitude: 126 + Math.random() * 4,
-        description: `Description ${i}`
-      });
-    }
-    return locations;
-  };
+const StyledTable = styled.table`
+  width: 100%;
+  border-collapse: collapse;
 
+  th,
+  td {
+    border: 1px solid #ccc;
+    padding: 8px;
+  }
+`;
+
+const StyledTableRow = styled.tr`
+  cursor: pointer;
+  transition: background-color 0.3s ease;
+  max-height: 50px;
+  overflow: hidden;
+
+  &:hover {
+    background-color: #f0f0f0;
+  }
+
+  &:active {
+    background-color: #c5c5c5;
+  }
+`;
+
+const AddLocation = ({ isOpen, setIsOpen, dayIndex, locationIndex }) => {
   const { currentRoute } = useCurrentRouteData();
   const [tripLocation, setTripLocation] = useState(null);
   const [tab, setTab] = useState("map");
-  const [recommendedLocations, setRecommendedLocations] = useState(generateRecommendedLocations());
+
+
+  const [selectedRegion, setSelectedRegion] = useState("서울"); // State for selected region
+  const [recommendedLocations, setRecommendedLocations] = useState([]);
 
   const [inputName, setInputName] = useState("");
   const [inputLatitude, setInputLatitude] = useState("");
   const [inputLongitude, setInputLongitude] = useState("");
+  const [inputDescription, setInputDescription] = useState("");
   const [mapPosition, setMapPosition] = useState({ lat: 36.3, lng: 127.4 });
 
   useEffect(() => {
     if (isOpen && currentRoute?.plannersByDay?.[dayIndex]) {
-      let _tripLocation = 
+      let _tripLocation =
       {
         day: dayIndex,
         latitude: 36.3,
@@ -98,16 +118,14 @@ const AddLocation = ({ isOpen, setIsOpen, dayIndex, locationIndex }) => {
         touristDestinationName: "New Location"
       };
 
-      if(currentRoute.plannersByDay[dayIndex].length > 0)
-      {
-        _tripLocation = currentRoute.plannersByDay[dayIndex][locationIndex-1];
+      if (currentRoute.plannersByDay[dayIndex].length > 0) {
+        _tripLocation = currentRoute.plannersByDay[dayIndex][locationIndex - 1];
       }
 
       setMapPosition({
         lat: _tripLocation.latitude,
         lng: _tripLocation.longitude
       });
-      console.log(_tripLocation);
 
       setTripLocation(_tripLocation);
     }
@@ -120,6 +138,13 @@ const AddLocation = ({ isOpen, setIsOpen, dayIndex, locationIndex }) => {
       setInputLongitude(tripLocation.longitude);
     }
   }, [tripLocation]);
+
+  useEffect(() => {
+    if (selectedRegion) {
+      // Fetch data based on selected region
+      fetchRegionData(selectedRegion);
+    }
+  }, [selectedRegion]);
 
   const handleNameChange = (e) => {
     setInputName(e.target.value);
@@ -145,18 +170,49 @@ const AddLocation = ({ isOpen, setIsOpen, dayIndex, locationIndex }) => {
     });
   };
 
+  const handleDescriptionChange = (e) => {
+    setInputDescription(e.target.value);
+    setTripLocation({
+      ...tripLocation,
+      longitude: e.target.value
+    });
+  }
+
   const saveLocation = () => {
     currentRoute.plannersByDay[dayIndex][locationIndex] = tripLocation;
     setIsOpen(false);
   }
 
-  return <Modal isVisible={isOpen} onClose={() => setIsOpen(false)} width="1000px" height="600px">
+  const fetchRegionData = async (region) => {
+    try {
+      const response = await axios.get(`http://localhost:8080/api/tourlist?area=${region}`);
+      if (response.status === 200) {
+        const regionsData = response.data.map((location, index) => {
+          return {
+            id: index,
+            name: location.touristDestinationName,
+            address: "주소 임시 홀더",
+            latitude: location.latitude,
+            longitude: location.longitude,
+            description: location.introduction
+          };
+        });
+        setRecommendedLocations(regionsData);
+      } else {
+        console.error('Failed to fetch data');
+      }
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    }
+  };
+
+  return <Modal isVisible={isOpen} onClose={() => setIsOpen(false)} width="1000px" height="700px">
     <ModalWrapper>
-      <h2>Add Location</h2>
+      <h2>여행지 추가</h2>
       <TabButtonContainer>
         {/* Tab buttons */}
-        <TabButton onClick={() => setTab("map")}>Select location from map</TabButton>
-        <TabButton onClick={() => setTab("recommended")}>Select recommended location</TabButton>
+        <TabButton onClick={() => setTab("map")}>지도</TabButton>
+        <TabButton onClick={() => setTab("recommended")}>추천 여행지</TabButton>
       </TabButtonContainer>
 
       {/* Render content based on selected tab */}
@@ -166,7 +222,7 @@ const AddLocation = ({ isOpen, setIsOpen, dayIndex, locationIndex }) => {
             <div>
               <Map
                 center={{ lat: mapPosition?.lat || 128, lng: mapPosition?.lng || 37 }}
-                style={{ width: '500px', height: '400px' }}
+                style={{ width: '500px', height: '500px' }}
                 level={3}
                 onClick={(_, mouseEvent) => {
                   let locationName = tripLocation.name;
@@ -198,6 +254,7 @@ const AddLocation = ({ isOpen, setIsOpen, dayIndex, locationIndex }) => {
                           longitude: pos.getLng(),
                           touristDestinationName: locationName
                         });
+                        setInputDescription("");
                       });
 
 
@@ -208,58 +265,61 @@ const AddLocation = ({ isOpen, setIsOpen, dayIndex, locationIndex }) => {
                 }}
               >
                 <MapMarker position={{
-                  lat: tripLocation?.latitude || 128,
-                  lng: tripLocation?.longitude || 37
+                  lat: tripLocation?.latitude || 126.3,
+                  lng: tripLocation?.longitude || 36.4
                 }} />
               </Map>
             </div>
           )}
           {tab === 'recommended' && (
             <div>
-              <h3>Recommended Locations</h3>
-              <table>
-                <thead>
-                  <tr>
-                    <th>Location Name</th>
-                    <th>Address</th>
-                    <th>Description</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  {recommendedLocations.map((location) => (
-                    <tr key={location.id} onClick={() => {
-                      console.log(location);
-                      setTripLocation(
-                        {
+              <TabButtonContainer>
+                {['서울', '부산', '대구', '인천', '광주', '대전', '울산', '세종', '경기', '강원', '충북', '충남', '전북', '전남', '경북', '경남', '제주'].map((region) => (
+                  <TabButton key={region} onClick={() => setSelectedRegion(region)}>{region}</TabButton>
+                ))}
+              </TabButtonContainer>
+              <div style={{ overflowX: 'hidden', overflowY: 'auto', maxHeight: '450px' }}>
+                <StyledTable style={{ minWidth: '100%', borderCollapse: 'collapse' }}>
+                  <thead>
+                    <tr>
+                      <th width="160px">여행지명</th>
+                      <th>주소</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {recommendedLocations.map((location) => (
+                      <StyledTableRow key={location.id} onClick={() => {
+                        setTripLocation({
                           day: tripLocation.day,
                           latitude: location.latitude,
                           longitude: location.longitude,
                           touristDestinationName: location.name ? location.name : location.address
-                        }
-                      )
-                      setMapPosition({
-                        lat: location.latitude,
-                        lng: location.longitude
-                      });
-                    }}>
-                      <td>{location.name}</td>
-                      <td>{location.address}</td>
-                      <td>{location.description}</td>
-                    </tr>
-                  ))}
-                </tbody>
-              </table>
+                        });
+                        setInputDescription(location.description);
+                        setMapPosition({
+                          lat: location.latitude,
+                          lng: location.longitude
+                        });
+                      }}>
+                        <td height="50px">{location.name}</td>
+                        <td>{location.address}</td>
+                      </StyledTableRow>
+                    ))}
+                  </tbody>
+                </StyledTable>
+              </div>
             </div>
           )}
         </TabContentWrapper>
         <LocationInputFormWrapper>
-          <h3>Name</h3>
+          <h3>여행지명</h3>
           <input style={{ width: "420px" }} placeholder='name' value={inputName} onChange={handleNameChange}></input>
-          <h3>Position</h3>
           <input style={{ width: "200px", margin: "0 15px 0 0" }} placeholder='latitude'
-            value={inputLatitude} onChange={handleLatitudeChange}></input>
+            value={inputLatitude} onChange={handleLatitudeChange} hidden></input>
           <input style={{ width: "200px", margin: "0 15px 0 0" }} placeholder='longitude'
-            value={inputLongitude} onChange={handleLongitudeChange}></input>
+            value={inputLongitude} onChange={handleLongitudeChange} hidden></input>
+          <h3>설명</h3>
+          <p>{inputDescription}</p>
         </LocationInputFormWrapper>
       </ContentWrapper>
 
